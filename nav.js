d97b58d0b7b4793d82473290cb082dbd58a359bd
Faiz <220701167@rajalakshmi.edu.in>
2024-08-20 17:44:52 +0530

    initial-commit with nav-bar

--- nav.js
+++ nav.js
@@ -1,7 +1,15 @@
-const hamburger = document.querySelector('.hamburger')
+const hamburger = document.querySelector('.hamburger-icon')
 const navlinks = document.querySelector('.nav-links')
+const crossbtn = document.querySelector(' .hamburger-icon i')
 
-hamburger.addEventListener('click' , () => {
-    hamburger.classList.toggle('active')
-    navlinks.classList.toggle('active')
+hamburger.addEventListener('click' , (e) => {
+    navlinks.classList.toggle('open')
+
+    const isOpen =  navlinks.classList.contains('open')
+    crossbtn.setAttribute('class' , isOpen ? "ri-close-line":"ri-menu-line")
+})
+
+navlinks.addEventListener('click'  , (e) => {
+    navlinks.classList.remove('open');
+    crossbtn.setAttribute('class' , 'ri-menu-line')
 })
