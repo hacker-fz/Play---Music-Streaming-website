e2e77dd59ef6784bf054e715ec82fc69007f38d9
Faiz <220701167@rajalakshmi.edu.in>
2024-08-21 18:24:13 +0530

    adding songs data

---
+++ data/songs.js
@@ -0,0 +1,352 @@
+const songs = [
+    {
+        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        image: "images/music-cards/save your tears",
+        name: "Save Your Tears",
+        artist: "The Weeknd",
+        listeners: '126k',
+        keywords: [
+            "Weekend",
+            "Save your tears",
+            "sad songs"
+        ]
+    },
+    {
+        id: "a12347ce-1bb0-4f95-c29d-d2f08eb789d7",
+        image: "images/music-cards/blinding lights",
+        name: "Blinding Lights",
+        artist: "The Weeknd",
+        listeners: '200k',
+        keywords: [
+            "Weekend",
+            "Blinding Lights",
+            "pop",
+            "synthwave"
+        ]
+    },
+    {
+        id: "b23458df-2cc1-5e06-d39e-e3f19fc890e8",
+        image: "images/music-cards/levitating",
+        name: "Levitating",
+        artist: "Dua Lipa",
+        listeners: '180k',
+        keywords: [
+            "Dua Lipa",
+            "Levitating",
+            "dance-pop",
+            "feel-good"
+        ]
+    },
+    {
+        id: "c34569ef-3dd2-6f17-e40f-f4g21hd901f9",
+        image: "images/music-cards/driver's license",
+        name: "Driver's License",
+        artist: "Olivia Rodrigo",
+        listeners: '250k',
+        keywords: [
+            "Olivia Rodrigo",
+            "Driver's License",
+            "breakup",
+            "emotional"
+        ]
+    },
+    {
+        id: "d45670fg-4ee3-7g28-f51g-g5h32je012g0",
+        image: "images/music-cards/montero",
+        name: "Montero (Call Me By Your Name)",
+        artist: "Lil Nas X",
+        listeners: '220k',
+        keywords: [
+            "Lil Nas X",
+            "Montero",
+            "LGBTQ",
+            "controversial"
+        ]
+    },
+    {
+        id: "e56781gh-5ff4-8h39-g62h-h6i43kf123h1",
+        image: "images/music-cards/peaches",
+        name: "Peaches",
+        artist: "Justin Bieber",
+        listeners: '195k',
+        keywords: [
+            "Justin Bieber",
+            "Peaches",
+            "R&B",
+            "summer vibes"
+        ]
+    },
+    {
+        id: "f67892ij-6gg5-9i40-h73i-i7j54lg234i2",
+        image: "images/music-cards/good-4-u",
+        name: "Good 4 U",
+        artist: "Olivia Rodrigo",
+        listeners: '240k',
+        keywords: [
+            "Olivia Rodrigo",
+            "Good 4 U",
+            "pop-punk",
+            "angst"
+        ]
+    },
+    {
+        id: "g78903jk-7hh6-0j51-i84j-j8k65mh345j3",
+        image: "images/music-cards/kiss-me-more",
+        name: "Kiss Me More",
+        artist: "Doja Cat",
+        listeners: '210k',
+        keywords: [
+            "Doja Cat",
+            "Kiss Me More",
+            "R&B",
+            "summer"
+        ]
+    },
+    {
+        id: "h89014kl-8ii7-1k62-j95k-k9l76ni456k4",
+        image: "images/music-cards/stay",
+        name: "Stay",
+        artist: "The Kid LAROI, Justin Bieber",
+        listeners: '270k',
+        keywords: [
+            "The Kid LAROI",
+            "Justin Bieber",
+            "Stay",
+            "pop"
+        ]
+    },
+    {
+        id: "i90125lm-9jj8-2l73-k06l-l0m87oj567l5",
+        image: "images/music-cards/deja-vu",
+        name: "Deja Vu",
+        artist: "Olivia Rodrigo",
+        listeners: '230k',
+        keywords: [
+            "Olivia Rodrigo",
+            "Deja Vu",
+            "pop",
+            "nostalgia"
+        ]
+    },
+    {
+        id: "j01236mn-0kk9-3m84-l17m-m1n98pk678m6",
+        image: "images/music-cards/industry-baby",
+        name: "Industry Baby",
+        artist: "Lil Nas X, Jack Harlow",
+        listeners: '260k',
+        keywords: [
+            "Lil Nas X",
+            "Jack Harlow",
+            "Industry Baby",
+            "rap"
+        ]
+    },
+    {
+        id: "k12347no-1ll0-4n95-m28n-n2o09ql789n7",
+        image: "images/music-cards/happier-than-ever",
+        name: "Happier Than Ever",
+        artist: "Billie Eilish",
+        listeners: '280k',
+        keywords: [
+            "Billie Eilish",
+            "Happier Than Ever",
+            "emotional",
+            "pop"
+        ]
+    },
+    {
+        id: "m34569pq-3nn2-6p17-o40p-p4q21sn901p9",
+        image: "images/music-cards/shivers",
+        name: "Shivers",
+        artist: "Ed Sheeran",
+        listeners: '250k',
+        keywords: [
+            "Ed Sheeran",
+            "Shivers",
+            "pop",
+            "romance"
+        ]
+    },
+    {
+        id: "n45670qr-4oo3-7q28-p51q-q5r32to012q0",
+        image: "images/music-cards/bad-habits",
+        name: "Bad Habits",
+        artist: "Ed Sheeran",
+        listeners: '290k',
+        keywords: [
+            "Ed Sheeran",
+            "Bad Habits",
+            "pop",
+            "catchy"
+        ]
+    },
+    {
+        id: "o56781rs-5pp4-8r39-q62r-r6s43up123r1",
+        image: "images/music-cards/easy-on-me",
+        name: "Easy On Me",
+        artist: "Adele",
+        listeners: '320k',
+        keywords: [
+            "Adele",
+            "Easy On Me",
+            "ballad",
+            "emotional"
+        ]
+    },
+    {
+        id: "p67892st-6qq5-9s40-r73s-s7t54vq234s2",
+        image: "images/music-cards/heat-waves",
+        name: "Heat Waves",
+        artist: "Glass Animals",
+        listeners: '270k',
+        keywords: [
+            "Glass Animals",
+            "Heat Waves",
+            "indie",
+            "vibes"
+        ]
+    },
+    {
+        id: "q78903tu-7rr6-0t51-s84t-t8u65wr345t3",
+        image: "images/music-cards/leave-the-door-open",
+        name: "Leave The Door Open",
+        artist: "Silk Sonic, Bruno Mars",
+        listeners: '290k',
+        keywords: [
+            "Silk Sonic",
+            "Bruno Mars",
+            "Leave The Door Open",
+            "R&B"
+        ]
+    },
+    {
+        id: "r89014uv-8ss7-1u62-t95u-u9v76xs456u4",
+        image: "images/music-cards/pov",
+        name: "pov",
+        artist: "Ariana Grande",
+        listeners: '240k',
+        keywords: [
+            "Ariana Grande",
+            "pov",
+            "love",
+            "ballad"
+        ]
+    },
+    {
+        id: "s90125vw-9tt8-2v73-u06v-v0w87yt567v5",
+        image: "images/music-cards/fearless-taylor's-version",
+        name: "Fearless (Taylor's Version)",
+        artist: "Taylor Swift",
+        listeners: '310k',
+        keywords: [
+            "Taylor Swift",
+            "Fearless",
+            "re-recording",
+            "country-pop"
+        ]
+    },
+    {
+        id: "t01236wx-0uu9-3w84-v17w-w1x98zu678w6",
+        image: "images/music-cards/telepatia",
+        name: "Telepatía",
+        artist: "Kali Uchis",
+        listeners: '260k',
+        keywords: [
+            "Kali Uchis",
+            "Telepatía",
+            "Latin",
+            "dreamy"
+        ]
+    },
+    {
+        id: "u12347xy-1vv0-4x95-w28x-x2y09av789x7",
+        image: "images/music-cards/levitating-remix",
+        name: "Levitating (Remix)",
+        artist: "Dua Lipa, DaBaby",
+        listeners: '280k',
+        keywords: [
+            "Dua Lipa",
+            "Levitating",
+            "Remix",
+            "DaBaby"
+        ]
+    },
+    {
+        id: "v23458yz-2ww1-5y06-x39y-y3z10bw890y8",
+        image: "images/music-cards/you-right",
+        name: "You Right",
+        artist: "Doja Cat, The Weeknd",
+        listeners: '230k',
+        keywords: [
+            "Doja Cat",
+            "The Weeknd",
+            "You Right",
+            "R&B"
+        ]
+    },
+    {
+        id: "w34569za-3xx2-6z17-y50z-z4a21cp901z9",
+        image: "images/music-cards/good-days",
+        name: "Good Days",
+        artist: "SZA",
+        listeners: '275k',
+        keywords: [
+            "SZA",
+            "Good Days",
+            "R&B",
+            "introspective"
+        ]
+    },
+    {
+        id: "x45670ab-4yy3-7a28-z61a-a5b32dq012a0",
+        image: "images/music-cards/mood",
+        name: "Mood",
+        artist: "24kGoldn, Iann Dior",
+        listeners: '295k',
+        keywords: [
+            "24kGoldn",
+            "Iann Dior",
+            "Mood",
+            "pop-rap"
+        ]
+    },
+    {
+        id: "y56781bc-5zz4-8b39-a72b-b6c43er123b1",
+        image: "images/music-cards/positions",
+        name: "positions",
+        artist: "Ariana Grande",
+        listeners: '310k',
+        keywords: [
+            "Ariana Grande",
+            "positions",
+            "pop",
+            "love"
+        ]
+    },
+    {
+        id: "z67892cd-6aa5-9c40-b83c-c7d54fs234c2",
+        image: "images/music-cards/savage-love",
+        name: "Savage Love (Laxed - Siren Beat)",
+        artist: "Jawsh 685, Jason Derulo",
+        listeners: '280k',
+        keywords: [
+            "Jawsh 685",
+            "Jason Derulo",
+            "Savage Love",
+            "TikTok"
+        ]
+    },
+    {
+        id: "b89014ef-8cc7-1e62-d05e-e9f76hu456e4",
+        image: "images/music-cards/astronaut-in-the-ocean",
+        name: "Astronaut In The Ocean",
+        artist: "Masked Wolf",
+        listeners: '290k',
+        keywords: [
+            "Masked Wolf",
+            "Astronaut In The Ocean",
+            "rap",
+            "viral"
+        ]
+    }
+];
--- scripts/homepage.js
+++ scripts/homepage.js
@@ -30,4 +30,16 @@ const scrollRevealOption = {
     delay: 600,
   });
 
+  const cards = document.querySelectorAll('.card-container');
 
+  cards.forEach(card => {
+      card.addEventListener("mousemove", (e) => {
+          const rect = card.getBoundingClientRect();
+          const x = e.clientX - rect.left - rect.width / 2;
+          const y = e.clientY - rect.top - rect.height / 2;
+      
+          const angle = Math.atan2(-x, y);
+          card.style.setProperty("--rotation", angle + "rad");
+      });
+  });
+  
